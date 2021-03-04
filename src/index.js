7278bfd792a4e75f57d3984f5ee9fc736e06e810
Vladimir Kilinich <kylynych.v@gmail.com>
2021-03-04 21:46:01 +0200

    добавляет рендер импута на главную

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.scss';
+import refs from './js/refs';
+import inputTemplate from './templates/header/home.hbs';
+
+const input = inputTemplate();
+
+document.body.insertAdjacentHTML('beforeend', input);
