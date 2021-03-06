a84e27c9e735de4862b6b1abb14eb51a86421157
olenaLa <olelapti32@gmail.com>
2021-03-07 01:07:57 +0200

    markup team

---
+++ src/js/list-devs.js
@@ -0,0 +1,53 @@
+const students = [
+  {
+    src: 'https:',
+    name: 'Vladimir Kilinich',
+    role: 'Team Lead',
+  },
+  {
+    src: 'https:',
+    name: 'Vladyslav Hapon',
+    role: 'Scrum Master',
+  },
+  {
+    src: 'https:',
+    name: 'Aleksandr Pryadko',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Alena Laptieva',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Bogdan Remeniuk',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Filip Koza',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Liubov Shpakova',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Marina Permiakova',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Vladyslav Remeniuk',
+    role: 'Developer',
+  },
+  {
+    src: 'https:',
+    name: 'Volodymyr Kostetskyi',
+    role: 'Developer',
+  }
+];
+export default students;
