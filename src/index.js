33f26bc60a1237f2aa002a3acb0a6ed7683c92fa
olenaLa <olelapti32@gmail.com>
2021-03-06 19:35:54 +0200

    markup modal-team

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './styles.scss';
 import refs from './js/refs';
 import inputTemplate from './templates/header/home.hbs';
 
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import './js/modal-team';
+
 const input = inputTemplate();
 
 refs.header.insertAdjacentHTML('beforeend', input);
---
+++ src/js/modal-team.js
@@ -0,0 +1,36 @@
+import * as basicLightbox from 'basiclightbox';
+import students from './list-devs';
+
+
+const markupStudent = students.reduce(
+  (string, { src, link, name, role }) =>
+    string +
+    `
+<div class="team-card">
+ <img class="team-image" src="${src}" alt="${name}">
+    <h2 class="team-name">${name}</h2>
+    <p class="team-role">${role}</p>
+</div>`,
+  '',
+);
+
+const container = document.querySelector('[data-index="team"]')
+
+container.addEventListener('click', openModalStudent);
+
+const modal = basicLightbox.create(
+  `<div class="team-wrapper">${markupStudent}</div>`,
+);
+
+function openModalStudent(e) {
+  modal.show();
+
+  window.addEventListener('keydown', closeModalHandler);
+
+  function closeModalHandler(e) {
+    if (e.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModalHandler);
+    }
+  }
+}
