38f2e7c835fc2a079e87146c2aefa39e8eca45cb
Marina Permiakova <marinapermyakova13@gmail.com>
2021-03-09 09:43:57 +0200

    добавляет QUEUE
    1) addToQueueList.js - добавляет на local storage
    2) queueList.js - создает список при нажатии queue в my library
    3)в шаблоне модалки добавляет класс на кнопку
    4) сверху в файлах написано как их подключать

---
+++ src/js/addToQueueList.js
@@ -0,0 +1,32 @@
+// В INDEX.JS 
+// 1) зверху:
+// import addToQueueList from './js/addToQueueList';
+
+// 2) в function modalMatchesFounder в самий кінець, орієнтовно рядок 171:
+//  addToQueueList(modalGenreEditor(currentFilmObj, genreDB));
+
+function addToQueueList(element) {
+  createQueueLocalStorage();
+  const { poster_path, original_title, genre_ids, release_date } = element;
+  const data = { poster_path, original_title, genre_ids, release_date };
+  const addToQueueBtn = document.querySelector('.js-queue');
+  addToQueueBtn.addEventListener('click', () => {
+    const storage = localStorage.getItem('queue');
+    if (storage.includes(element.poster_path)) {
+      alert('This film has already been added;)');
+      return;
+    }
+    const newStorage = JSON.parse(localStorage.getItem('queue'));
+    newStorage.push(data);
+    localStorage.setItem('queue', JSON.stringify(newStorage));
+  });
+}
+
+function createQueueLocalStorage() {
+  const storage = localStorage.getItem('queue');
+  if (storage === null) {
+    return localStorage.setItem('queue', JSON.stringify([]));
+  }
+}
+
+export default addToQueueList;
