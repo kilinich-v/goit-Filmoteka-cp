9e6841abea797e2769b294c08933b960183b6ba2
Marina Permiakova <marinapermyakova13@gmail.com>
2021-03-10 15:13:51 +0200

    Обновляет queue
    -удаляет пустой массив
    -добавляет изменения кнопки queue
    -вешает disabled на кнопку

--- src/js/addToQueueList.js
+++ src/js/addToQueueList.js
@@ -1,25 +1,32 @@
 function addToQueueList(element) {
-  createQueueLocalStorage();
   const { poster_path, original_title, genre_ids, release_date } = element;
   const data = { poster_path, original_title, genre_ids, release_date };
   const addToQueueBtn = document.querySelector('.js-queue');
+  const storage = localStorage.getItem('queue');
+  if (storage) {
+    if (storage.includes(element.original_title)) {
+      btnStatus('In queue', true);
+      return;
+    }
+  }
+
   addToQueueBtn.addEventListener('click', () => {
-    const storage = localStorage.getItem('queue');
-    if (storage.includes(element.poster_path)) {
-      alert('This film has already been added;)');
+    if (!storage) {
+      localStorage.setItem('queue', JSON.stringify([data]));
+      btnStatus('In Queue', true);
       return;
     }
     const newStorage = JSON.parse(localStorage.getItem('queue'));
     newStorage.push(data);
     localStorage.setItem('queue', JSON.stringify(newStorage));
+    btnStatus('In Queue', true);
   });
 }
 
-function createQueueLocalStorage() {
-  const storage = localStorage.getItem('queue');
-  if (storage === null) {
-    return localStorage.setItem('queue', JSON.stringify([]));
-  }
-}
-
 export default addToQueueList;
+
+function btnStatus(text, status) {
+  const addToQueueBtn = document.querySelector('.js-queue');
+  addToQueueBtn.textContent = text;
+  addToQueueBtn.disabled = status;
+}
