2bcd59f2da6999bb00b0dc3110e443935a283948
Marina Permiakova <marinapermyakova13@gmail.com>
2021-03-12 15:57:38 +0200

    добавляет рендеринг на queue и правильное удаление с local storage

--- src/js/addToQueueList.js
+++ src/js/addToQueueList.js
@@ -1,11 +1,15 @@
-// import storage from './libraryControll';
-// import createQueueListFn from './queueList';
 import refs from './refs';
-import filmGalleryTemplate from '../templates/filmgallery.hbs';
+import rendering from './rendering';
+import deletingFromLocalStorage from './deletingFromLocaleStorage';
 
 function addToQueueList(element) {
   const { poster_path, original_title, genre_ids, release_date } = element;
-  const data = { poster_path, original_title, genre_ids, release_date };
+  const data = {
+    poster_path,
+    original_title,
+    genre_ids,
+    release_date,
+  };
 
   const addToQueueBtn = document.querySelector('.js-queue');
   const watchedBtn = document.querySelector('.js-watched');
@@ -18,93 +22,64 @@ function addToQueueList(element) {
   }
 
   addToQueueBtn.addEventListener('click', () => {
+    deleteWatched(element);
     document.querySelector('.js-watched').classList.remove('added-to-watched');
     document.querySelector('.js-watched').textContent = 'add to watched';
-    if (!addToQueueBtn.disabled) {
-      addToQueueBtn.addEventListener(
-        'click',
-        deletingFromLocalStorage('watched', element),
-      );
-      document.querySelector('.js-watched').textContent = 'add to watched';
-
-      document
-        .querySelector('.js-watched')
-        .classList.remove('added-to-storage');
-      document.querySelector('.js-queue').classList.add('added-to-storage');
-      if (document.querySelector('[data-index="watched"]')) {
-        refs.galleryRef.textContent = '';
-        refs.galleryRef.insertAdjacentHTML(
-          'beforeend',
-          filmGalleryTemplate(JSON.parse(localStorage.getItem('watched'))),
-        );
-
-        if (!JSON.parse(localStorage.getItem('watched'))) {
-          refs.galleryRef.insertAdjacentHTML(
-            'afterbegin',
-            'No Watched moovies to show',
-          );
-        }
-      }
-    }
     addToQueueBtn.classList.add('added-to-watched');
     btnText(addToQueueBtn);
     if (!localStorage.getItem('queue')) {
       return localStorage.setItem('queue', JSON.stringify([data]));
     }
+
+    if (localStorage.getItem('queue').includes(element.poster_path)) {
+      addToQueueBtn.classList.remove('added-to-watched');
+      btnText(addToQueueBtn);
+      deletingFromLocalStorage('queue', element);
+      renderingQueue();
+      return;
+    }
     const newStorage = JSON.parse(localStorage.getItem('queue'));
     newStorage.push(data);
-    const dataArray = JSON.parse(localStorage.getItem('watched'));
-
-    if (refs.myLibraryBtn.classList.contains('current')) {
-      rendering(dataArray);
-    }
-
+    renderingQueue();
     return localStorage.setItem('queue', JSON.stringify(newStorage));
   });
 
   watchedBtn.addEventListener('click', () => {
     addToQueueBtn.classList.remove('added-to-watched');
-    btnText(addToQueueBtn);
-    deletingFromLocalStorage('queue', element);
-    document.querySelector('.js-queue').classList.remove('added-to-storage');
-
-    if (document.querySelector('[data-index="watched"]')) {
-      refs.galleryRef.textContent = '';
-      refs.galleryRef.insertAdjacentHTML(
-        'beforeend',
-        filmGalleryTemplate(JSON.parse(localStorage.getItem('queue'))),
-      );
-      if (!JSON.parse(localStorage.getItem('queue'))) {
-        refs.galleryRef.insertAdjacentHTML(
-          'afterbegin',
-          'No moovies in Queue to show',
-        );
-      }
+    if (!localStorage.getItem('queue')) {
+      return;
     }
+    if (localStorage.getItem('queue').includes(element.poster_path)) {
+      btnText(addToQueueBtn);
+      deletingFromLocalStorage('queue', element);
+    }
+
+    renderingQueue();
   });
-}
-function deletingFromLocalStorage(key, element) {
-  const newStorage = JSON.parse(localStorage.getItem(key));
-  if (!localStorage.getItem(key)) {
-    return;
-  }
-  if (newStorage.length < 2) {
-    localStorage.removeItem(key);
-    return;
-  }
-  const updatedStorage = newStorage.filter(
-    el => el.poster_path !== element.poster_path,
-  );
-  return localStorage.setItem(key, JSON.stringify(updatedStorage));
 }
 
 function btnText(btn) {
   if (btn.classList.contains('added-to-watched')) {
-    btn.textContent = 'In queue';
+    btn.textContent = 'in queue';
   } else {
-    btn.textContent = 'Add to queue';
+    btn.textContent = 'add to queue';
   }
   return btn.textContent;
+}
+
+function deleteWatched(element) {
+  if (!localStorage.getItem('watched')) {
+    return;
+  }
+  if (localStorage.getItem('watched').includes(element.poster_path)) {
+    deletingFromLocalStorage('watched', element);
+  }
+}
+
+function renderingQueue() {
+  if (refs.myLibraryBtn.classList.contains('current')) {
+    rendering('queue');
+  }
 }
 
 export default addToQueueList;
