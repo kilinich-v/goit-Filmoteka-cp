b60eee89f29f497faedf33eb9e3537263a11491d
Vladimir Kilinich <kylynych.v@gmail.com>
2021-05-08 19:49:34 +0300

    add events to modal

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import './js/libraryControll';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import './js/modal-team';
 import { getPopularFilms, getSearchingFilms } from './js/renderGallery';
-import { handleFilmModal, handleCloseModal } from './js/renderFilmModal';
+import {
+  handleCreateModal,
+  handleModalClick,
+  handleModalKeypress,
+} from './js/renderFilmModal';
 
 getPopularFilms();
 
 refs.inputSubmit().addEventListener('submit', getSearchingFilms);
-refs.gallery.addEventListener('click', handleFilmModal);
-refs.backdrop.addEventListener('click', handleCloseModal);
+refs.gallery.addEventListener('click', handleCreateModal);
+refs.backdrop.addEventListener('click', handleModalClick);
+window.addEventListener('keydown', handleModalKeypress);
--- src/js/renderFilmModal.js
+++ src/js/renderFilmModal.js
@@ -3,24 +3,52 @@ import refs from './refs';
 import apiService from './apiService';
 import modalTemplate from '../templates/modal.hbs';
 
-function renderModal(filmData) {
-  const modalMarkup = modalTemplate(filmData);
-
-  refs.modal.insertAdjacentHTML('afterbegin', modalMarkup);
-}
-
-export function handleFilmModal(event) {
+export function handleCreateModal(event) {
   event.preventDefault();
 
-  if (event.target.dataset.movieid) {
-    apiService.movieID = event.target.dataset.movieid;
+  if (!event.target?.dataset?.movieid) {
+    return;
   }
 
+  apiService.movieID = event.target.dataset.movieid;
   apiService.fetchMovieInfo().then(data => {
     renderModal(data);
-    console.log(refs.backdrop);
-    refs.backdrop.classList.remove('is-hidden');
   });
 }
 
-export function handleCloseModal(event) {}
+export function handleModalClick(event) {
+  const target = event.target;
+
+  if (target.classList.value === 'backdrop') {
+    closeModal();
+  }
+
+  if (target.id === 'close') {
+    closeModal();
+  }
+}
+
+export function handleModalKeypress(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+}
+
+function renderModal(filmData) {
+  const modalMarkup = modalTemplate(filmData);
+
+  refs.modal.innerHTML = '';
+  refs.modal.insertAdjacentHTML('afterbegin', modalMarkup);
+
+  openModal();
+}
+
+function closeModal() {
+  refs.backdrop.classList.add('is-hidden');
+  refs.body.classList.remove('modal-open');
+}
+
+function openModal() {
+  refs.backdrop.classList.remove('is-hidden');
+  refs.body.classList.add('modal-open');
+}
